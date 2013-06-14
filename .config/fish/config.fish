01a1f2a7f1e47c9db850bb02beb5e0885a6a55a7
Shintaro Abe <dictav@gmail.com>
2013-06-14 16:13:06 +0900

    Change shell from bash to fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,51 @@
+set -x BROWSER open
+set -x EDITOR /Applications/MacVim.app/Contents/MacOS/Vim
+set -x PATH ~/bin /usr/local/bin /Applications/MacVim.app/Contents/MacOS $PATH
+set -x GEM_HOME /usr/local/lib/ruby/gems/2.0.0
+set -x PATH $PATH /usr/local/lib/ruby/gems/2.0.0/bin
+
+# key bindings
+function fish_user_key_bindings
+   . ~/.config/fish/functions/vi-mode.fish
+   vi_mode_insert
+end
+
+# jekyll alias
+function jekyll
+   set -lx BUNDLE_GEMFILE ~/.jekyll/Gemfile
+   bundle exec jekyll $argv
+end
+
+# rails aliases
+function r_new
+   set -lx BUNDLE_GEMFILE ~/.rails/Gemfile
+   bundle exec rails new $argv
+end
+
+function randle
+   set -lx GEM_HOME ~/.rails/ruby/2.0.0
+   bundle $argv
+end
+
+alias hoge "randle list"
+alias raxec "randle exec"
+alias bails "raxec rails"
+alias bake "raxec rake"
+alias buard "raxec guard"
+alias bspec "raxec rspec"
+
+# vim aliases
+alias vi "Vim"
+alias vim "mvim"
+alias diff "mvimdiff"
+
+# pod command
+function pod
+   set -lx BUNDLE_GEMFILE ~/.cocoapods/Gemfile
+   bundle exec pod $argv
+end
+
+function pod-update
+   set -lx BUNDLE_GEMFILE ~/.cocoapods/Gemfile
+   bundle update
+end
---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,24 @@
+function parse_git_dirty 
+   git diff —quiet HEAD ^&-
+   if test $status = 1
+      echo (set_color $fish_git_dirty_color)”Δ”(set_color normal)
+   end
+end
+
+function parse_git_branch
+   # git branch outputs lines, the current branch is prefixed with a *
+   set -l branch (git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/\1/") 
+   echo $branch (parse_git_dirty)     
+end
+
+function fish_prompt
+   echo 
+   set_color white
+   echo [(hostname):(pwd)]
+   set_color yellow
+   echo (parse_git_branch)
+   set_color white
+   echo -n (whoami)'$ '
+   set_color normal
+end
+
